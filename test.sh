49c14e83f2a86e0bcb48562744853167a5b8c9f4
fullth <xoghksdla@gmail.com>
2024-03-25 22:20:36 +0900

    update: README.md

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
- npm start
-  echo "npm start 스크립트 실행 완료"
-  git diff --quiet HEAD README.md
-  echo $?
-  if [ $? -eq 0 ]; then
-    echo "변경사항 없음"
-  else
-    git add .
-    git config --local user.email "xoghksdla@gmail.com"
-    git config --local user.name "fullth"
-    git commit -m "update: README.md"
-    git push
-    echo "커밋 후 푸쉬"
-  fi
+npm start
+echo "npm start 스크립트 실행 완료"
+git diff --quiet HEAD README.md
+diff_exit_code=$?
+echo $diff_exit_code
+if [ $diff_exit_code -eq 0 ]; then
+  echo "변경사항 없음"
+else
+  git add .
+  git config --local user.email "xoghksdla@gmail.com"
+  git config --local user.name "fullth"
+  git commit -m "update: README.md"
+  git push
+  echo "커밋 후 푸쉬"
+fi
